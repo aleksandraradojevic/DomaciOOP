3c7806343a3afc95f39d74117d3588b63fc4bcb1
Aleksandra Radojevic <aleksandra.radojevic93@gmail.com>
2022-01-14 17:49:44 +0100

    Domaci 13/01/2022

--- src/d_11_01_2022/FacebookPost.java
+++ src/d_11_01_2022/FacebookPost.java
@@ -2,6 +2,15 @@ package d_11_01_2022;
 
 public class FacebookPost {
 
+	public FacebookPost(String nameFrom, String nameTo, String text) {
+		this.nameFrom = nameFrom;
+		this.nameTo = nameTo;
+		this.text = text;
+	}
+
+	public FacebookPost() {
+	}
+
 	String nameFrom;
 	String nameTo;
 	String text;
@@ -31,6 +40,38 @@ public class FacebookPost {
 		System.out.println(this.text);
 		System.out.println("Likes " + this.like + " | Shares " + this.share);
 
+	}
+
+	public String getNameFrom() {
+		return nameFrom;
+	}
+
+	public void setNameFrom(String nameFrom) {
+		this.nameFrom = nameFrom;
+	}
+
+	public String getNameTo() {
+		return nameTo;
+	}
+
+	public void setNameTo(String nameTo) {
+		this.nameTo = nameTo;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public int getLike() {
+		return like;
+	}
+
+	public int getShare() {
+		return share;
 	}
 
 }
--- src/d_11_01_2022/Zadatak3.java
+++ src/d_11_01_2022/Zadatak3.java
@@ -4,18 +4,26 @@ public class Zadatak3 {
 
 	public static void main(String[] args) {
 
-		FacebookPost fb1 = new FacebookPost();
-		fb1.nameFrom = "Pera Peric";
-		fb1.nameTo = "Mika Mikic";
-		fb1.text= "Hello!";
-		fb1.like = 5;
-		fb1.share = 1;
-		
-		
+		FacebookPost fb1 = new FacebookPost("Pera Peric", "Zika Zikic", "Hello!");
+
+		fb1.like();
+		fb1.like();
+		fb1.like();
+		fb1.like();
+		fb1.share();
 		fb1.dislike();
 		fb1.share();
 		
 		fb1.print();
+		
+		FacebookPost fb2 = new FacebookPost("Mika Mikic", "Pera Peric", "Bye bye!");
+
+		fb2.like();
+		fb2.like();
+		fb2.share();
+		fb2.dislike();
+		
+		fb2.print();
 
 	}
 
